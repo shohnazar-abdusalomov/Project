2b37dc64f07e7e57b1c239b2ffbb917263c20c45
Safaralievna <farangizshaykulova06@gmail.com>
2026-02-25 13:07:14 +0500

    All Farangiz's tasks done

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Header from "./components/header/Header";
 import Hero from "./components/hero/Hero";
 import Footer from "./components/footer/Footer"
 import "./App.css";
+import Latest from "./components/Latest/Latest.jsx";
 
 
 
@@ -9,6 +10,7 @@ function App() {
 	return (
 		<div className="container">
 			<Header/>
+			<Latest/>
 			<Hero/>
 			<Footer/>
 		</div>
